ab86e1e63b0af9f4cfea8f2e1664086925bc9019
bstardev\tung.trinh <tung.trinh@bstarsolutions.com>
2022-11-07 11:43:24 +0700

    Update env prod

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -5,4 +5,10 @@ export const environment = {
   API_URL: "https://med-service-demo.herokuapp.com/api/",
   DOMAIN_SHARING: "",
   SVG_HOST: "../assets/svg",
+  FORMAT_SETTING: {
+    date: "DD/MM/YYYY",
+    time: "HH:mm:ss a",
+    dateTime: "DD/MM/YYYY HH:mm:ss",
+    number: "0,00,###",
+  }
 };
